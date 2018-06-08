d86d7e941e433d753c0313f96e9d9355cb8af08b
Hirohito Higashi <uzuki.local>
2018-06-08 19:19:04 +0900

    add PSoC5LP I2C device class.

---
+++ PSoC5LP/i2c/c_i2c.h
@@ -0,0 +1,30 @@
+/*! @file
+  @brief
+  I2C class for Cypress PSoC5LP
+
+  <pre>
+  Copyright (C) 2018 Kyushu Institute of Technology.
+  Copyright (C) 2018 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+
+  </pre>
+*/
+
+#ifndef MRBC_I2C_PSOC_H_
+#define MRBC_I2C_PSOC_H_
+
+#include "value.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void mrbc_init_class_i2c(struct VM *vm);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
