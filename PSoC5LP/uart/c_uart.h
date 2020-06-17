1a133f203dd834a76f911b0d54a502cae854443a
HirohitoHigashi <higashi@s-itoc.jp>
2020-06-17 10:17:23 +0900

    follow mruby/c master changes.

--- PSoC5LP/uart/c_uart.h
+++ PSoC5LP/uart/c_uart.h
@@ -3,8 +3,8 @@
   UART class for Cypress PSoC5LP
 
   <pre>
-  Copyright (C) 2018 Kyushu Institute of Technology.
-  Copyright (C) 2018 Shimane IT Open-Innovation Center.
+  Copyright (C) 2018-2020 Kyushu Institute of Technology.
+  Copyright (C) 2018-2020 Shimane IT Open-Innovation Center.
 
   This file is distributed under BSD 3-Clause License.
 
@@ -14,13 +14,11 @@
 #ifndef MRBC_PSOC5LP_UART_H_
 #define MRBC_PSOC5LP_UART_H_
 
-#include "value.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+struct VM;
 void mrbc_init_class_uart(struct VM *vm);
 
 
